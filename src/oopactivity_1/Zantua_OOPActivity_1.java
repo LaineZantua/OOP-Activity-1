79eeb7a849801bd667c8250e975c80b659437397
Sam <Sam@LAPTOP-5MCQS3EL>
2024-03-15 13:39:18 +0800

    OOP Activity 1

---
+++ src/oopactivity_1/Zantua_OOPActivity_1.java
@@ -0,0 +1,52 @@
+package oopactivity_1;
+
+import java.util.Scanner;
+
+public class Zantua_OOPActivity_1 {
+
+    public static void main(String[] args) {
+        java.util.Scanner data = new Scanner (System.in);
+        
+        String firstName;
+        String middleInitial;
+        String lastName;
+        String gender;
+        int age;
+        int month;
+        int day;
+        int year;
+      
+        
+        
+        System.out.println("First Name: ");
+        firstName = data.nextLine();
+        
+        System.out.println("Middle Initial: ");
+        middleInitial = data.nextLine();
+        
+        System.out.println("Last Name: ");
+        lastName = data.nextLine();
+
+        System.out.println("Gender: ");
+        gender = data.nextLine();
+        
+        System.out.println("Age: ");
+        age = data.nextInt();
+        
+        System.out.println("Month:");
+        month = data.nextInt();         
+        
+        System.out.println("Day: ");
+        day = data.nextInt();
+        
+        System.out.println("Year: ");
+        year = data.nextInt();
+        
+        
+        System.out.println("First Name: " + firstName + "\n" + "Middle Initial: "
+                    + middleInitial + "\n" + "Last Name: " + lastName + "\n" + 
+                "Gender: " + gender + "\n" + "Age: " + age + "\n" + "Month: " 
+        + month + "\n" + "Day: " + day + "\n" + "Year: " + year);
+    }
+    
+}
